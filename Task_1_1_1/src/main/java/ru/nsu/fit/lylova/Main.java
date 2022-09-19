3af6347df7fe2aa7c124e889c60966c83fca8f7d
SophyaL <sophyal@mail.ru>
2022-09-19 18:55:34 +0700

    add class for heapsort

--- Task_1_1_1/src/main/java/ru/nsu/fit/lylova/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/fit/lylova/Main.java
@@ -1,45 +1,28 @@
 package ru.nsu.fit.lylova;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
+/**
+ * Class with function main
+ */
 public class Main {
-
-    public static int[] heapsort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n; ++i) {
-            int v = i + 1;
-            while (v != 1) {
-                if (arr[v / 2 - 1] < arr[v - 1]) {
-                    int x = arr[v / 2 - 1];
-                    arr[v / 2 - 1] = arr[v - 1];
-                    arr[v - 1] = x;
-                }
-                v /= 2;
-            }
-        }
-        for (int i = n - 1; i > 0; --i) {
-            int x = arr[i];
-            arr[i] = arr[0];
-            arr[0] = x;
-            int v = 1;
-            while (v * 2 < i + 1) {
-                int id_of_biggest = v * 2 + 1 < i + 1 && arr[v * 2 + 1 - 1] > arr[v * 2 - 1] ? v * 2 + 1 : v * 2;
-                if (arr[v - 1] < arr[id_of_biggest - 1]) {
-                    x = arr[v - 1];
-                    arr[v - 1] = arr[id_of_biggest - 1];
-                    arr[id_of_biggest - 1] = x;
-                    v = id_of_biggest;
-                } else {
-                    break;
-                }
-            }
-        }
-        return arr;
-    }
-
+    /**
+     * Asks you to enter an array of integers via the command line.
+     * Then outputs this array in ascending order.
+     * The heapsort algorithm is used to sort the array.
+     *
+     * @param args arguments of command line (not used)
+     */
     public static void main(String[] args) {
-        int[] a = {5, 2, 1, 4, 1};
-        System.out.println("init array            : " + Arrays.toString(a));
-        System.out.println("my realization of heap: " + Arrays.toString(heapsort(a)));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array of integers: ");
+        String s = sc.nextLine();
+        String[] as = s.split(" ");
+        int[] a = new int[as.length];
+        for (int i = 0; i < as.length; ++i) {
+            a[i] = Integer.parseInt(as[i]);
+        }
+        System.out.println("Sorted array: " + Arrays.toString(Heapsort.heapsort(a)));
     }
 }
