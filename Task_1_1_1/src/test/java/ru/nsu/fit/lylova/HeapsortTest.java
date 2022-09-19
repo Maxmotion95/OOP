50fd449d524fe0099f98b05637b7adb52c724781
SophyaL <sophyal@mail.ru>
2022-09-19 18:56:31 +0700

    add tests for heapsort

---
+++ Task_1_1_1/src/test/java/ru/nsu/fit/lylova/HeapsortTest.java
@@ -0,0 +1,50 @@
+package ru.nsu.fit.lylova;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeapsortTest {
+
+    @Test
+    void small_arrays_test() {
+        int[][] init_arrays = {{1, 2, 3, 4, 5},
+                          {8, 4, 2},
+                          {8, 8, 8, 8},
+                          {2, 3, 9, 0},
+                          {-10, 100, 25, 37},
+                          {637, 43627, 3547, 534, 33432, 143, 243, 25421}};
+        int[][] heapsorted = new int[init_arrays.length][];
+        int[][] arrays = init_arrays;
+        for (int i = 0; i < arrays.length; ++i){
+            heapsorted[i] = Heapsort.heapsort(arrays[i]);
+            Arrays.sort(arrays[i]);
+        }
+        for (int i = 0; i < arrays.length; ++i){
+            assertEquals(arrays[i], heapsorted[i],
+                    "Array: " + Arrays.toString(init_arrays[i]) +
+                            ", Expected answer: " + Arrays.toString(arrays[i]) +
+                            ", Heapsort result: " + Arrays.toString(heapsorted[i]) + "\n");
+        }
+    }
+
+    @RepeatedTest(10)
+    void large_arrays_test() {
+        Random random = new Random();
+        int n = 10000 + random.nextInt(10000);
+        int[] init_a = new int[n];
+        for (int i = 0; i < n; ++i){
+            init_a[i] = random.nextInt();
+        }
+        int[] sorted = init_a, heapsorted = Heapsort.heapsort(init_a);
+        Arrays.sort(sorted);
+        assertEquals(sorted, heapsorted,
+                "Array: " + Arrays.toString(init_a) +
+                        ", Expected answer: " + Arrays.toString(sorted) +
+                        ", Heapsort result: " + Arrays.toString(heapsorted) + "\n");
+    }
+}
