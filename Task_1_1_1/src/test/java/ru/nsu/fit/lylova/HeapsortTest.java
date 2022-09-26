6427a33f198fe6a3fa556a5da0c7368d42bebb49
SophyaL <sophyal@mail.ru>
2022-09-26 09:39:22 +0700

    another try to start workflow

--- Task_1_1_1/src/test/java/ru/nsu/fit/lylova/HeapsortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/fit/lylova/HeapsortTest.java
@@ -1,57 +1,57 @@
 package ru.nsu.fit.lylova;
 
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Random;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HeapsortTest {
 
-    static Stream<int[]> intArrayProvider() {
-        return Stream.of(
-                new int[]{1, 2, 3, 4, 5},
-                new int[]{8, 4, 2},
-                new int[]{8, 8, 8, 8},
-                new int[]{2, 3, 9, 0},
-                new int[]{-10, 100, 25, 37},
-                new int[]{637, 43627, 3547, 534, 33432, 143, 243, 25421});
-    }
+    @Test
+    void small_arrays_test() {
+        int[][] init_arrays = {{1, 2, 3, 4, 5},
+                          {8, 4, 2},
+                          {8, 8, 8, 8},
+                          {2, 3, 9, 0},
+                          {-10, 100, 25, 37},
+                          {637, 43627, 3547, 534, 33432, 143, 243, 25421}};
 
-    @ParameterizedTest
-    @MethodSource("intArrayProvider")
-    void smallArraysTest(int[] array) {
-        int[] heapSorted = array.clone();
-        int[] sorted = array.clone();
-        Heapsort.heapsort(heapSorted);
-        Arrays.sort(sorted);
-        for (int i = 0; i < array.length; ++i) {
-            assertEquals(sorted[i], heapSorted[i],
-                    "Initial array was: " + Arrays.toString(array));
+        int[][] heapsorted = init_arrays.clone();
+        int[][] arrays = init_arrays.clone();
+        for (int i = 0; i < arrays.length; ++i){
+            Heapsort.heapsort(heapsorted[i]);
+            Arrays.sort(arrays[i]);
+        }
+        for (int i = 0; i < arrays.length; ++i){
+            for (int j = 0; j < arrays[i].length; ++j){
+                assertEquals(arrays[i][j], heapsorted[i][j],
+                        "Array: " + Arrays.toString(init_arrays[i]) +
+                                ", Expected answer: " + Arrays.toString(arrays[i]) +
+                                ", Heapsort result: " + Arrays.toString(heapsorted[i]) + "\n");
+            }
         }
     }
 
     @RepeatedTest(10)
-    void largeArraysTest() {
+    void large_arrays_test() {
         Random random = new Random();
         int n = 1000 + random.nextInt(1000);
         int[] init_a = new int[n];
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i){
             init_a[i] = random.nextInt();
         }
         int[] sorted = init_a.clone();
-        int[] heapSorted = init_a.clone();
-        Heapsort.heapsort(heapSorted);
+        int[] heapsorted = init_a.clone();
+        Heapsort.heapsort(heapsorted);
         Arrays.sort(sorted);
-        for (int i = 0; i < n; ++i) {
-            assertEquals(sorted[i], heapSorted[i],
+        for (int i = 0; i < n; ++i){
+            assertEquals(sorted[i], heapsorted[i],
                     "Array: " + Arrays.toString(init_a) +
                             ", Expected answer: " + Arrays.toString(sorted) +
-                            ", Heapsort result: " + Arrays.toString(heapSorted) + "\n");
+                            ", Heapsort result: " + Arrays.toString(heapsorted) + "\n");
         }
     }
 }
