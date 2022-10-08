568ba59a066198d51ac169c63e2a8755bfadc7ba
SophyaL <sophyal@mail.ru>
2022-10-08 23:20:16 +0700

    create Main class

--- Task_1_2_2/src/main/java/ru/nsu/fit/lylova/Main.java
+++ Task_1_2_2/src/main/java/ru/nsu/fit/lylova/Main.java
@@ -2,6 +2,16 @@ package ru.nsu.fit.lylova;
 
 public class Main {
     public static void main(String[] args) {
+        Tree<String> tree = new Tree<>("HEHEH");
+        Node<String> v = tree.addVertexByValue("kek");
+        Node<String> u = tree.addVertexByValue("foo");
+        tree.addVertexByParentAndValue(v, "1234");
+        tree.addVertexByParentAndValue(u, "76325");
+        System.out.println(v.value);
+        for (String s: tree){
+            System.out.print(s + " ");
+        }
+        System.out.println();
         System.out.println("Hello world!");
     }
 }
