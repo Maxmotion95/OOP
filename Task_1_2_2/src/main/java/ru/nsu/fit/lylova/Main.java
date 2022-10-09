847a5a90e38d4e5437b489b3de65878fffd33f47
SophyaL <sophyal@mail.ru>
2022-10-09 22:54:44 +0700

    done implementation of collection interface and Dfs, Bfs iterators

--- Task_1_2_2/src/main/java/ru/nsu/fit/lylova/Main.java
+++ Task_1_2_2/src/main/java/ru/nsu/fit/lylova/Main.java
@@ -1,14 +1,25 @@
 package ru.nsu.fit.lylova;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Tree<String> tree = new Tree<>("HEHEH");
+        Tree<String> tree = new Tree<>();
         Node<String> v = tree.addVertexByValue("kek");
         Node<String> u = tree.addVertexByValue("foo");
         tree.addVertexByParentAndValue(v, "1234");
         tree.addVertexByParentAndValue(u, "76325");
         System.out.println(v.value);
-        for (String s: tree){
+        for (String s : tree) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+        ArrayList<String> delete = new ArrayList<>();
+        delete.add("kek");
+        delete.add("76325");
+        tree.removeAll(delete);
+        System.out.println("After deleting:");
+        for (var s : tree) {
             System.out.print(s + " ");
         }
         System.out.println();
