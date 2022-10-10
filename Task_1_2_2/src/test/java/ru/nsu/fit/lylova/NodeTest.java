d28eb8fff84c104f124e89acfc5b51d6a7fdf58e
SophyaL <sophyal@mail.ru>
2022-10-10 12:15:25 +0700

    add tests for node class

--- Task_1_2_2/src/test/java/ru/nsu/fit/lylova/NodeTest.java
+++ Task_1_2_2/src/test/java/ru/nsu/fit/lylova/NodeTest.java
@@ -1,13 +1,11 @@
 package ru.nsu.fit.lylova;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-
-import java.util.Random;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class NodeTest {
 
@@ -24,28 +22,28 @@ class NodeTest {
     @Test
     void testConstructors() {
         Node<Integer> node = new Node<>();
-        assertNull(node.getParent());
-        assertNull(node.getValue());
-        assertNotEquals(null, node.getChildren());
-        assertEquals(0, node.getChildren().size());
+        assertNull(node.parent);
+        assertNull(node.value);
+        assertNotEquals(null, node.children);
+        assertEquals(0, node.children.size());
 
         node = new Node<>(1123);
-        assertNull(node.getParent());
-        assertEquals(1123, node.getValue());
-        assertNotEquals(null, node.getChildren());
-        assertEquals(0, node.childrenCount());
+        assertNull(node.parent);
+        assertEquals(1123, node.value);
+        assertNotEquals(null, node.children);
+        assertEquals(0, node.children.size());
 
         Node<Integer> node1 = new Node<>(node, 1342);
-        assertEquals(node, node1.getParent());
-        assertEquals(1342, node1.getValue());
-        assertNotEquals(null, node1.getChildren());
-        assertEquals(0, node1.childrenCount());
+        assertEquals(node, node1.parent);
+        assertEquals(1342, node1.value);
+        assertNotEquals(null, node1.children);
+        assertEquals(0, node1.children.size());
 
         node1 = new Node<>(node);
-        assertEquals(node, node1.getParent());
-        assertNull(node1.getValue());
-        assertNotEquals(null, node1.getChildren());
-        assertEquals(0, node1.childrenCount());
+        assertEquals(node, node1.parent);
+        assertNull(node1.value);
+        assertNotEquals(null, node1.children);
+        assertEquals(0, node1.children.size());
     }
 
     @Test
@@ -57,11 +55,11 @@ class NodeTest {
             arr[i] = random.nextInt();
             Node<Integer> newNode = new Node<>(arr[i]);
             node.addChild(newNode);
-            assertEquals(newNode, node.getChildren().get(i));
+            assertEquals(newNode, node.children.get(i));
         }
-        assertEquals(100, node.childrenCount());
+        assertEquals(100, node.children.size());
         for (int i = 0; i < 100; ++i) {
-            assertEquals(node, node.getChildren().get(i).getParent());
+            assertEquals(node, node.children.get(i).parent);
         }
     }
 
@@ -74,7 +72,7 @@ class NodeTest {
             arr[i] = random.nextInt();
             Node<Integer> newNode = new Node<>(arr[i]);
             node.addChild(newNode);
-            assertEquals(i + 1, node.childrenCount());
+            assertEquals(i + 1, node.cntChildren());
         }
     }
 }
