aa4db97313ebb1c708ed0905f4c8d464f5c0c5c3
SophyaL <sophyal@mail.ru>
2022-10-10 11:23:59 +0700

    add javadoc

--- Task_1_2_2/src/main/java/ru/nsu/fit/lylova/Node.java
+++ Task_1_2_2/src/main/java/ru/nsu/fit/lylova/Node.java
@@ -2,36 +2,75 @@ package ru.nsu.fit.lylova;
 
 import java.util.ArrayList;
 
+/**
+ * Class that implements the vertex of the tree.
+ *
+ * @param <T> type of value in {@code Node}
+ */
 public class Node<T> {
     public final ArrayList<Node<T>> children = new ArrayList<>();
     public T value = null;
     public Node<T> parent = null;
 
+    /**
+     * Creates empty vertex with {@code parent} = {@code null}, {@code value} = {@code null}.
+     */
     public Node() {
     }
 
+    /**
+     * Creates vertex with {@code parent} = {@code null} and specified value.
+     *
+     * @param value value of vertex
+     */
     public Node(T value) {
         this.value = value;
     }
 
+    /**
+     * Creates vertex with specified parent and {@code value} = {@code null}.
+     *
+     * @param parent parent of vertex
+     */
     public Node(Node<T> parent) {
         this.parent = parent;
     }
 
+    /**
+     * Creates vertex with specified parent and value.
+     *
+     * @param parent parent of vertex
+     * @param value  value of vertex
+     */
     public Node(Node<T> parent, T value) {
         this.parent = parent;
         this.value = value;
     }
 
+    /**
+     * Return value of vertex in string. Equals to {@code .value.toString()}.
+     *
+     * @return value of vertex in string
+     */
     @Override
     public String toString() {
         return value.toString();
     }
 
+    /**
+     * Add vertex {@code child} to {@code children} array.
+     *
+     * @param child child vertex
+     */
     public void addChild(Node<T> child) {
         this.children.add(child);
     }
 
+    /**
+     * Returns children count of vertex. Equals to {@code .children.size()}.
+     *
+     * @return children count of vertex
+     */
     public int cntChildren() {
         return this.children.size();
     }
--- Task_1_2_2/src/main/java/ru/nsu/fit/lylova/Tree.java
+++ Task_1_2_2/src/main/java/ru/nsu/fit/lylova/Tree.java
@@ -2,25 +2,49 @@ package ru.nsu.fit.lylova;
 
 import java.util.*;
 
+/**
+ * Class that implement collection tree. Tree is a connected, acyclic, oriented graph.
+ *
+ * @param <T> type of value in vertexes of tree
+ */
 public class Tree<T> implements Collection<T> {
     private final Node<T> root;
     private int cntNodes;
 
+    /**
+     * Creates empty tree
+     */
     public Tree() {
         root = new Node<>();
         cntNodes = 0;
     }
 
+    /**
+     * Returns number of vertices in tree
+     *
+     * @return number of vertices in the tree
+     */
     @Override
     public int size() {
         return cntNodes;
     }
 
+    /**
+     * Returns {@code true} if this collection contains no elements.
+     *
+     * @return {@code true} if this collection contains no elements.
+     */
     @Override
     public boolean isEmpty() {
         return cntNodes == 0;
     }
 
+    /**
+     * Add vertex with specified value to root.
+     *
+     * @param t element whose presence in this collection is to be ensured
+     * @return {@code true}
+     */
     @Override
     public boolean add(T t) {
         Node<T> newNode = new Node<>(root, t);
@@ -29,6 +53,12 @@ public class Tree<T> implements Collection<T> {
         return true;
     }
 
+    /**
+     * Returns true if this collection contains the specified element.
+     *
+     * @param o element whose presence in this collection is to be tested
+     * @return {@code true} if tree contain vertex with this object
+     */
     @Override
     public boolean contains(Object o) {
         for (T value : this) {
@@ -39,6 +69,12 @@ public class Tree<T> implements Collection<T> {
         return false;
     }
 
+    /**
+     * Returns an iterator over the elements in this collection.
+     * The order of the vertices corresponds to the DFS traversal.
+     *
+     * @return an {@code Iterator} over the elements in tree
+     */
     @Override
     public Iterator<T> iterator() {
         return new Iterator<>() {
@@ -56,6 +92,11 @@ public class Tree<T> implements Collection<T> {
         };
     }
 
+    /**
+     * Returns an array containing all elements in tree.
+     *
+     * @return array containing all elements in tree
+     */
     @Override
     public Object[] toArray() {
         Object[] res = new Object[cntNodes];
@@ -67,6 +108,18 @@ public class Tree<T> implements Collection<T> {
         return res;
     }
 
+    /**
+     * Returns an array containing all elements in tree; the runtime type of the returned array
+     * is that of the specified array. If the collection fits in the specified array,
+     * it is returned therein. Otherwise, a new array is allocated with the runtime type
+     * of the specified array and the size of tree.
+     *
+     * @param a    the array into which the elements of this collection are to be
+     *             stored, if it is big enough; otherwise, a new array of the same
+     *             runtime type is allocated for this purpose.
+     * @param <T1> Type of elements in specified collection
+     * @return array contains all elements of tree
+     */
     @SuppressWarnings("unchecked")
     @Override
     public <T1> T1[] toArray(T1[] a) {
@@ -84,6 +137,12 @@ public class Tree<T> implements Collection<T> {
         return a;
     }
 
+    /**
+     * Removes a single instance of the specified element from tree if it is present.
+     *
+     * @param o element to be removed from this collection, if present
+     * @return {@code true} if element deleted from tree
+     */
     @Override
     public boolean remove(Object o) {
         Iterator<Node<T>> iterator = new TreeDfsIterator();
@@ -101,6 +160,13 @@ public class Tree<T> implements Collection<T> {
         return false;
     }
 
+    /**
+     * Returns {@code true} if tree contains all elements
+     * in the specified collection.
+     *
+     * @param c collection to be checked for containment in this collection
+     * @return {@code true} if all elements contains in tree
+     */
     @Override
     public boolean containsAll(Collection<?> c) {
         for (var o : c) {
@@ -110,6 +176,12 @@ public class Tree<T> implements Collection<T> {
         return true;
     }
 
+    /**
+     * Adds all elements in the specified collection to tree.
+     *
+     * @param c collection containing elements to be added to this collection
+     * @return {@code true}
+     */
     @Override
     public boolean addAll(Collection<? extends T> c) {
         for (var o : c) {
@@ -118,6 +190,13 @@ public class Tree<T> implements Collection<T> {
         return true;
     }
 
+    /**
+     * Removes all of this collection's elements that are also contained in the
+     * tree.
+     *
+     * @param c collection containing elements to be removed from this collection
+     * @return {@code true} if all elements removed
+     */
     @Override
     public boolean removeAll(Collection<?> c) {
         for (var o : c) {
@@ -127,24 +206,43 @@ public class Tree<T> implements Collection<T> {
         return true;
     }
 
+    /**
+     * Retains only the elements in this collection that are contained in the tree.
+     * In other words, removes from tree all of its elements that are
+     * not contained in the specified collection.
+     *
+     * @param c collection containing elements to be retained in this collection
+     * @return {@code true} if tree changed as a result of the call
+     */
     @Override
     public boolean retainAll(Collection<?> c) {
         Iterator<Node<T>> iterator = new TreeDfsIterator();
+        boolean res = false;
         while (iterator.hasNext()) {
             Node<T> vertex = iterator.next();
             if (!c.contains(vertex.value)) {
                 this.remove(vertex);
+                res = true;
             }
         }
-        return true;
+        return res;
     }
 
+    /**
+     * Clear tree
+     */
     @Override
     public void clear() {
         root.children.clear();
         cntNodes = 0;
     }
 
+    /**
+     * Add vertex to tree that contains {@code value} and has parent root of tree.
+     *
+     * @param value value of vertex
+     * @return vertex that added
+     */
     public Node<T> addVertexByValue(T value) {
         Node<T> newNode = new Node<>(root, value);
         root.addChild(newNode);
@@ -152,6 +250,13 @@ public class Tree<T> implements Collection<T> {
         return newNode;
     }
 
+    /**
+     * Add vertex to tree that contains {@code value} and has parent {@code parent}.
+     *
+     * @param parent vertex that is parent of new vertex
+     * @param value  value of vertex
+     * @return vertex that added
+     */
     public Node<T> addVertexByParentAndValue(Node<T> parent, T value) {
         Node<T> newNode = new Node<>(parent, value);
         ++cntNodes;
@@ -159,6 +264,13 @@ public class Tree<T> implements Collection<T> {
         return newNode;
     }
 
+    /**
+     * Delete vertex from tree.
+     *
+     * @param vertex the vertex to be deleted
+     * @return {@code true} if vertex is deleted
+     * @throws Exception if vertex parent is null
+     */
     public boolean removeVertexByNode(Node<T> vertex) throws Exception {
         if (vertex.parent == null)
             throw new Exception();
@@ -171,12 +283,18 @@ public class Tree<T> implements Collection<T> {
         return true;
     }
 
+    /**
+     * An iterator that returns the elements of the tree in the order of DFS traversal.
+     */
     public class TreeDfsIterator implements Iterator<Node<T>> {
         private final Stack<Node<T>> stackNodes;
         private final Stack<Integer> pastId;
-        int idNow;
+        private int idNow;
         private Node<T> node;
 
+        /**
+         * Creates a tree iterator using a DFS traversal.
+         */
         public TreeDfsIterator() {
             stackNodes = new Stack<>();
             pastId = new Stack<>();
@@ -184,6 +302,12 @@ public class Tree<T> implements Collection<T> {
             idNow = -1;
         }
 
+        /**
+         * Returns true if the iteration has more elements. (In other words,
+         * returns true if next would return an element rather than throwing an exception.).
+         *
+         * @return true if the iteration has more elements
+         */
         @Override
         public boolean hasNext() {
             if (idNow + 1 != node.cntChildren())
@@ -195,6 +319,12 @@ public class Tree<T> implements Collection<T> {
             return !stackNodes.empty();
         }
 
+        /**
+         * Returns true if the iteration has more elements. (In other words,
+         * returns true if next would return an element rather than throwing an exception.).
+         *
+         * @return true if the iteration has more elements
+         */
         @Override
         public Node<T> next() throws NoSuchElementException {
             if (!this.hasNext()) {
@@ -214,17 +344,29 @@ public class Tree<T> implements Collection<T> {
         }
     }
 
+    /**
+     * An iterator that returns the elements of the tree in the order of BFS traversal.
+     */
     public class TreeBfsIterator implements Iterator<Node<T>> {
         private final Queue<Node<T>> queueNodes;
         private Node<T> nodeNow;
         private int idNow;
 
+        /**
+         * Creates a tree iterator using a BFS traversal.
+         */
         public TreeBfsIterator() {
             queueNodes = new LinkedList<>();
             nodeNow = root;
             idNow = -1;
         }
 
+        /**
+         * Returns true if the iteration has more elements. (In other words,
+         * returns true if next would return an element rather than throwing an exception.).
+         *
+         * @return true if the iteration has more elements
+         */
         @Override
         public boolean hasNext() {
             if (idNow + 1 != nodeNow.cntChildren())
@@ -236,6 +378,12 @@ public class Tree<T> implements Collection<T> {
             return idNow + 1 != nodeNow.cntChildren();
         }
 
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException – if the iteration has no more elements
+         */
         @Override
         public Node<T> next() throws NoSuchElementException {
             if (!this.hasNext())
