9911ead432d99759d431123fac754060a2bc5228
SophyaL <sophyal@mail.ru>
2022-10-22 12:03:38 +0700

    changes for checkstyle

--- Task_1_2_2/src/test/java/ru/nsu/fit/lylova/TreeTest.java
+++ Task_1_2_2/src/test/java/ru/nsu/fit/lylova/TreeTest.java
@@ -5,14 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Random;
+import org.junit.jupiter.api.Test;
+
 
 class TreeTest {
 
