a10385aef467f9df85db53a3c2b202e7de267f56
SophyaL <sophyal@mail.ru>
2022-10-23 19:59:43 +0700

    add implementation of example from task

--- Task_1_2_3/src/main/java/ru/nsu/fit/lylova/Main.java
+++ Task_1_2_3/src/main/java/ru/nsu/fit/lylova/Main.java
@@ -1,7 +1,81 @@
 package ru.nsu.fit.lylova;
 
 public class Main {
+
+    static class IntWeight implements GraphEdgeWeight<IntWeight>, Comparable<IntWeight> {
+        Integer weight;
+
+        IntWeight(){
+            weight = 0;
+        }
+
+        IntWeight(Integer w){
+            weight = w;
+        }
+
+        @Override
+        public IntWeight sum(IntWeight w) {
+            IntWeight res = new IntWeight(this.weight);
+            res.weight += w.weight;
+            return res;
+        }
+
+        @Override
+        public int compareTo(IntWeight o) {
+            if (this.weight < o.weight)
+                return -1;
+            if (this.weight.equals(o.weight))
+                return 0;
+            return 1;
+        }
+    }
+
+    static class Edge implements GraphEdge<IntWeight>{
+        Integer weight;
+
+        Edge(){
+            weight = 0;
+        }
+
+        Edge(Integer a){
+            weight = a;
+        }
+        @Override
+        public IntWeight getWeight() {
+            return new IntWeight(weight);
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Graph<String, Edge> g = new AdjacencyMatrixGraph<>();
+        String[] v = {"A", "B", "C", "D", "E", "F", "G"};
+        for (var i : v) {
+            g.addVertex(i);
+        }
+        g.addEdge("A", "B", new Edge(5));
+        g.addEdge("B", "A", new Edge(5));
+        g.addEdge("A", "D", new Edge(12));
+        g.addEdge("D", "A", new Edge(12));
+        g.addEdge("A", "G", new Edge(25));
+        g.addEdge("G", "A", new Edge(25));
+        g.addEdge("D", "B", new Edge(8));
+        g.addEdge("B", "D", new Edge(8));
+        g.addEdge("C", "D", new Edge(2));
+        g.addEdge("D", "C", new Edge(2));
+        g.addEdge("C", "E", new Edge(4));
+        g.addEdge("E", "C", new Edge(4));
+        g.addEdge("C", "F", new Edge(5));
+        g.addEdge("F", "C", new Edge(5));
+        g.addEdge("C", "G", new Edge(10));
+        g.addEdge("G", "C", new Edge(10));
+        g.addEdge("E", "G", new Edge(5));
+        g.addEdge("G", "E", new Edge(5));
+        g.addEdge("F", "G", new Edge(5));
+        g.addEdge("G", "F", new Edge(5));
+        ShortestPathInGraph<Graph<String, Edge>, String, Edge, IntWeight> a = new ShortestPathInGraph<>();
+        var ans = a.calc(g, "C", new IntWeight(0));
+        for (var i : v){
+            System.out.println(i + ": " + ans.get(i).weight);
+        }
+//        ShortestPathInGraph
     }
 }
