c831cea65dbfd626c4d6a564ba5700731629e465
SophyaL <sophyal@mail.ru>
2022-10-30 08:50:26 +0700

    change for checkstyle

--- Task_1_2_3/src/test/java/ru/nsu/fit/lylova/ShortestPathInGraphTest.java
+++ Task_1_2_3/src/test/java/ru/nsu/fit/lylova/ShortestPathInGraphTest.java
@@ -2,7 +2,7 @@ package ru.nsu.fit.lylova;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ShortestPathInGraphTest {
 
