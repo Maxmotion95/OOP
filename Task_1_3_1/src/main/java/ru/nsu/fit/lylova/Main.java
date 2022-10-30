a5cf0f360467b837a47b82af49d046149bad3945
SophyaL <sophyal@mail.ru>
2022-10-30 21:41:19 +0700

    realization finding string occurrences

--- Task_1_3_1/src/main/java/ru/nsu/fit/lylova/Main.java
+++ Task_1_3_1/src/main/java/ru/nsu/fit/lylova/Main.java
@@ -1,7 +1,26 @@
 package ru.nsu.fit.lylova;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+            throws IOException {
+        Scanner sc = new Scanner(System.in, StandardCharsets.UTF_8);
+        String filename = sc.nextLine();
+
+        File file = new File(filename);
+        BufferedReader input = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8));
+
+        String str;
+        str = sc.nextLine();
+        var ans = FindAllOccurrences.calc(input, str);
+        for (var i : ans) {
+            System.out.println(i);
+        }
     }
 }
