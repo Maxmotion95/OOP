1746fe567b1db677557992bc0f27b08383204920
SophyaL <sophyal@mail.ru>
2022-10-30 17:10:47 +0700

    add javadoc

--- Task_1_2_3/src/main/java/ru/nsu/fit/lylova/Graph/AdjacencyListGraph.java
+++ Task_1_2_3/src/main/java/ru/nsu/fit/lylova/Graph/AdjacencyListGraph.java
@@ -5,15 +5,30 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Implementation of {@code Graph} interface, that stores edges using adjacency list.
+ *
+ * @param <V> class of vertexes
+ * @param <E> class of edge value
+ */
 public class AdjacencyListGraph<V, E> implements Graph<V, E> {
     private final List<AdjacencyListEdge> listEdges;
     private final Set<V> vertexes;
 
+    /**
+     * Constructs an empty graph.
+     */
     public AdjacencyListGraph() {
         listEdges = new ArrayList<>();
         vertexes = new HashSet<>();
     }
 
+    /**
+     * Add vertex with value {@code v} to graph.
+     *
+     * @param v value of new vertex
+     * @return {@code true} if vertex was added
+     */
     @Override
     public boolean addVertex(V v) {
         if (vertexes.contains(v)) {
@@ -23,6 +38,12 @@ public class AdjacencyListGraph<V, E> implements Graph<V, E> {
         return true;
     }
 
+    /**
+     * Remove vertex with value {@code v} from graph.
+     *
+     * @param v value of vertex to remove
+     * @return {@code true} if graph was changed
+     */
     @Override
     public boolean removeVertex(V v) {
         if (!vertexes.contains(v)) {
@@ -39,6 +60,15 @@ public class AdjacencyListGraph<V, E> implements Graph<V, E> {
         return true;
     }
 
+    /**
+     * Add edge from vertex {@code a} to vertex {@code b}.
+     * Return {@code true} if edge was added.
+     *
+     * @param a start vertex of edge
+     * @param b end vertex of edge
+     * @param e value of edge
+     * @return {@code true} if edge was added
+     */
     @Override
     public boolean addEdge(V a, V b, E e) {
         if (!vertexes.contains(a) || !vertexes.contains(b)) {
@@ -49,6 +79,14 @@ public class AdjacencyListGraph<V, E> implements Graph<V, E> {
         return true;
     }
 
+    /**
+     * Remove edge from vertex {@code a} to vertex {@code b}.
+     * Return {@code true} if edge was removed.
+     *
+     * @param a start vertex of edge
+     * @param b end vertex of edge
+     * @return {@code true} if edge was removed
+     */
     @Override
     public boolean removeEdge(V a, V b) {
         for (var e : listEdges) {
@@ -60,11 +98,25 @@ public class AdjacencyListGraph<V, E> implements Graph<V, E> {
         return false;
     }
 
+    /**
+     * Returns ArrayList that contains all vertexes.
+     *
+     * @return ArrayList that contains all vertexes
+     */
     @Override
     public ArrayList<V> arrayVertexes() {
         return new ArrayList<>(vertexes);
     }
 
+    /**
+     * Return value of edge that starts from vertex with value {@code a} and
+     * ends in vertex with value {@code b}.
+     *
+     * @param a start vertex of edge
+     * @param b end vertex of edge
+     * @return value of edge that starts from vertex {@code a} and ends in vertex {@code b}
+     * @throws java.lang.Exception such edge doesn't exist
+     */
     @Override
     public E getEdge(V a, V b) throws Exception {
         if (!vertexes.contains(a) || !vertexes.contains(b)) {
--- Task_1_2_3/src/main/java/ru/nsu/fit/lylova/Graph/AdjacencyMatrixGraph.java
+++ Task_1_2_3/src/main/java/ru/nsu/fit/lylova/Graph/AdjacencyMatrixGraph.java
@@ -6,15 +6,30 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * Implementation of {@code Graph} interface, that stores edges using adjacency matrix.
+ *
+ * @param <V> class of vertexes
+ * @param <E> class of edge value
+ */
 public class AdjacencyMatrixGraph<V, E> implements Graph<V, E> {
     private final Set<V> vertexes;
     private final Map<V, Map<V, E>> matrix;
 
+    /**
+     * Constructs an empty graph.
+     */
     public AdjacencyMatrixGraph() {
         vertexes = new HashSet<>();
         matrix = new HashMap<>();
     }
 
+    /**
+     * Add vertex with value {@code v} to graph.
+     *
+     * @param v value of new vertex
+     * @return {@code true} if vertex was added
+     */
     @Override
     public boolean addVertex(V v) {
         if (vertexes.contains(v))
@@ -30,6 +45,12 @@ public class AdjacencyMatrixGraph<V, E> implements Graph<V, E> {
         return true;
     }
 
+    /**
+     * Remove vertex with value {@code v} from graph.
+     *
+     * @param v value of vertex to remove
+     * @return {@code true} if graph was changed
+     */
     @Override
     public boolean removeVertex(V v) {
         if (!vertexes.contains(v)) {
@@ -43,6 +64,16 @@ public class AdjacencyMatrixGraph<V, E> implements Graph<V, E> {
         return true;
     }
 
+    /**
+     * Add edge from vertex {@code a} to vertex {@code b}.
+     * If value of edge {@code e} equals {@code null} then edge will be removed.
+     * Return {@code true} if edge was added.
+     *
+     * @param a start vertex of edge
+     * @param b end vertex of edge
+     * @param e value of edge
+     * @return {@code true} if edge was added
+     */
     @Override
     public boolean addEdge(V a, V b, E e) {
         if (!vertexes.contains(a) || !vertexes.contains(b) || matrix.get(a).get(b) != null) {
@@ -52,6 +83,14 @@ public class AdjacencyMatrixGraph<V, E> implements Graph<V, E> {
         return true;
     }
 
+    /**
+     * Remove edge from vertex {@code a} to vertex {@code b}.
+     * Return {@code true} if edge was removed.
+     *
+     * @param a start vertex of edge
+     * @param b end vertex of edge
+     * @return {@code true} if edge was removed
+     */
     @Override
     public boolean removeEdge(V a, V b) {
         if (!vertexes.contains(a) || !vertexes.contains(b) || matrix.get(a).get(b) == null) {
@@ -61,11 +100,25 @@ public class AdjacencyMatrixGraph<V, E> implements Graph<V, E> {
         return true;
     }
 
+    /**
+     * Returns ArrayList that contains all vertexes.
+     *
+     * @return ArrayList that contains all vertexes
+     */
     @Override
     public ArrayList<V> arrayVertexes() {
         return new ArrayList<>(vertexes);
     }
 
+    /**
+     * Return value of edge that starts from vertex with value {@code a} and
+     * ends in vertex with value {@code b}.
+     *
+     * @param a start vertex of edge
+     * @param b end vertex of edge
+     * @return value of edge that starts from vertex {@code a} and ends in vertex {@code b}
+     * @throws java.lang.Exception such edge doesn't exist
+     */
     @Override
     public E getEdge(V a, V b) throws java.lang.Exception {
         if (!vertexes.contains(a) || !vertexes.contains(b)) {
