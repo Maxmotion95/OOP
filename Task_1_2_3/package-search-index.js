b11d09f3dcc0fb5c390ce0e5189e68597834a9d6
SophyaL <SophyaL@users.noreply.github.com>
2022-10-30 10:38:07 +0000

    Deploying to gh-pages from @ SophyaL/OOP@bc85d2634580434510e90c113718fe648af4bfa0 🚀

--- Task_1_2_3/package-search-index.js
+++ Task_1_2_3/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.fit.lylova"},{"l":"ru.nsu.fit.lylova.graph"},{"l":"ru.nsu.fit.lylova.graphAlgorithms"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.fit.lylova"},{"l":"ru.nsu.fit.lylova.algorithms"},{"l":"ru.nsu.fit.lylova.graph"}]
