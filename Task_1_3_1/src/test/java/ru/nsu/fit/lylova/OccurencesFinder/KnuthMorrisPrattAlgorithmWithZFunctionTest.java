465ce14f38f340677c7da6fdc5090ba48bc7001f
SophyaL <sophyal@mail.ru>
2022-11-20 11:52:08 +0700

    add interface for OccurrencesFinder

---
+++ Task_1_3_1/src/test/java/ru/nsu/fit/lylova/OccurencesFinder/KnuthMorrisPrattAlgorithmWithZFunctionTest.java
@@ -0,0 +1,38 @@
+package ru.nsu.fit.lylova.OccurencesFinder;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import ru.nsu.fit.lylova.Main;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KnuthMorrisPrattAlgorithmWithZFunctionTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1})
+    void mainTest(int testNumber) throws IOException {
+        File textFile = new File("src/test/resources/texts/text" + testNumber + ".txt");
+        File patternFile = new File("src/test/resources/patterns/pattern" + testNumber + ".txt");
+
+        InputStream textInputStream = new FileInputStream(textFile);
+
+        Scanner patternScanner = new Scanner(patternFile, StandardCharsets.UTF_8);
+        String pattern = patternScanner.nextLine();
+
+        var result = KnuthMorrisPrattAlgorithmWithZFunction.find(textInputStream, pattern);
+
+        File answerFile = new File("src/test/resources/answers/answer" + testNumber + ".txt");
+        Scanner answerScanner = new Scanner(answerFile);
+
+        int i = 0;
+        while (answerScanner.hasNextInt() && i < result.size()) {
+            assertEquals(answerScanner.nextInt(), result.get(i));
+            ++i;
+        }
+        assertTrue(!answerScanner.hasNextInt() && i == result.size());
+    }
+}
