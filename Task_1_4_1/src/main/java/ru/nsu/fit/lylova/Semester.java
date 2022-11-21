ff8e1820dfb1a071292fb885b8cecdd7943d51c2
SophyaL <sophyal@mail.ru>
2022-11-21 22:32:57 +0700

    add classes Gradebook, Person, Semester, Subject, Teacher

---
+++ Task_1_4_1/src/main/java/ru/nsu/fit/lylova/Semester.java
@@ -0,0 +1,28 @@
+package ru.nsu.fit.lylova;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Semester {
+    private List<Subject> subjects;
+
+    Semester(){
+        subjects = new ArrayList<>();
+    }
+
+    Semester(List<Subject> subjects) {
+        this.subjects = subjects;
+    }
+
+    public List<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(List<Subject> subjects) {
+        this.subjects = subjects;
+    }
+
+    public void addSubject(Subject subject) {
+        subjects.add(subject);
+    }
+}
