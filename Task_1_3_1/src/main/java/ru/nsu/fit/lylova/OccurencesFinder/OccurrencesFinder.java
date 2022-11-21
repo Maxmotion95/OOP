77e68940bc71b5d5739fe56f110cf3577e2c99c8
SophyaL <sophyal@mail.ru>
2022-11-21 14:04:15 +0700

    add javadoc

--- Task_1_3_1/src/main/java/ru/nsu/fit/lylova/OccurencesFinder/OccurrencesFinder.java
+++ Task_1_3_1/src/main/java/ru/nsu/fit/lylova/OccurencesFinder/OccurrencesFinder.java
@@ -4,7 +4,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
+/**
+ * The interface of a class that has a find function
+ * that finds all occurrences of a string in the text.
+ */
 public interface OccurrencesFinder {
+    /**
+     * Function that finds all occurrences of string {@code pattern}
+     * in the text that is contained in the stream {@code inputStream}.
+     *
+     * @param inputStream stream that contains text
+     * @param pattern pattern string
+     * @return list of indexes of all occurrences
+     * @throws IOException when inputStream throws IOException
+     */
     static ArrayList<Integer> find(InputStream inputStream, String pattern) throws IOException {
         return null;
     }
