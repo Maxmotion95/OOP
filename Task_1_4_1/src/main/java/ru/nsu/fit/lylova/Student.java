b873d2666c2898c830bdd4e8f8f21142f3381777
SophyaL <sophyal@mail.ru>
2022-11-21 22:33:41 +0700

    change class Student

--- Task_1_4_1/src/main/java/ru/nsu/fit/lylova/Student.java
+++ Task_1_4_1/src/main/java/ru/nsu/fit/lylova/Student.java
@@ -1,18 +1,13 @@
 package ru.nsu.fit.lylova;
 
-public class Student {
-    private String name = "";
-    private String surname = "";
-    private String patronymic = "";
+public class Student extends Person {
     private int group = 0;
 
     Student(){
     }
 
     Student(String name, String surname, String patronymic, int group) {
-        this.name = name;
-        this.surname = surname;
-        this.patronymic = patronymic;
+        super(name, surname, patronymic);
         this.group = group;
     }
 
@@ -22,29 +17,5 @@ public class Student {
 
     public void setGroup(int group) {
         this.group = group;
-    }
-
-    public String getPatronymic() {
-        return patronymic;
-    }
-
-    public void setPatronymic(String patronymic) {
-        this.patronymic = patronymic;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
