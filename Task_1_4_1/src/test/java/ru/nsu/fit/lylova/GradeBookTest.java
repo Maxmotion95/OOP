493286467b1cd17e1f71edab0cff2bcf6fe52d5c
SophyaL <sophyal@mail.ru>
2022-12-03 22:38:22 +0700

    change switch expression

--- Task_1_4_1/src/test/java/ru/nsu/fit/lylova/GradeBookTest.java
+++ Task_1_4_1/src/test/java/ru/nsu/fit/lylova/GradeBookTest.java
@@ -1,15 +1,17 @@
 package ru.nsu.fit.lylova;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.junit.jupiter.api.Test;
-import org.json.*;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
-class GradebookTest {
+class GradeBookTest {
 
     @Test
     void test() throws IOException {
@@ -19,7 +21,10 @@ class GradebookTest {
 
         GradeBook gradebook = new GradeBook(root);
         assertEquals("4,8", String.format("%.1f", gradebook.getAverageGrade()));
-        //System.out.println(gradebook.getAverageGrade());
+        assertTrue(gradebook.diplomaWithHonours());
+//        assertTrue(false);
+
+//        System.out.println(gradebook.getAverageGrade());
     }
 
 }
