710c17cf6a06386ec4a1f504d85ec65c09ac58c2
SophyaL <sophyal@mail.ru>
2022-12-03 21:55:32 +0700

    add test of getAverageGrade

--- Task_1_4_1/src/main/java/ru/nsu/fit/lylova/Main.java
+++ Task_1_4_1/src/main/java/ru/nsu/fit/lylova/Main.java
@@ -2,6 +2,11 @@ package ru.nsu.fit.lylova;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student kek = new Student("aba", "kek", "patr", 1224);
+        kek = new Student();
+        System.out.println(kek.getName());
+        System.out.println(kek.getSurname());
+        System.out.println(kek.getPatronymic());
+        System.out.println(kek.getGroup());
     }
 }
