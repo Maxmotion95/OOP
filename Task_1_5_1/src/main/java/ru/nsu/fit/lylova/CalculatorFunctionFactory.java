c864e69ab94b5649540666cf3f5a0fff2d4186a7
SophyaL <sophyal@mail.ru>
2022-12-10 20:59:17 +0700

    add all functions

--- Task_1_5_1/src/main/java/ru/nsu/fit/lylova/CalculatorFunctionFactory.java
+++ Task_1_5_1/src/main/java/ru/nsu/fit/lylova/CalculatorFunctionFactory.java
@@ -1,8 +1,6 @@
 package ru.nsu.fit.lylova;
 
-import ru.nsu.fit.lylova.functions.FunctionAdd;
-import ru.nsu.fit.lylova.functions.FunctionMultiply;
-import ru.nsu.fit.lylova.functions.FunctionSub;
+import ru.nsu.fit.lylova.functions.*;
 
 public class CalculatorFunctionFactory {
     static public CalculatorFunction createFunction(FunctionType type) {
@@ -17,6 +15,24 @@ public class CalculatorFunctionFactory {
                 break;
             case MULTIPLY:
                 function = new FunctionMultiply();
+                break;
+            case SIN:
+                function = new FunctionSin();
+                break;
+            case COS:
+                function = new FunctionCos();
+                break;
+            case DIVIDE:
+                function = new FunctionDivide();
+                break;
+            case LOG:
+                function = new FunctionLog();
+                break;
+            case SQRT:
+                function = new FunctionSqrt();
+                break;
+            case POW:
+                function = new FunctionPow();
                 break;
             default:
                 throw new IllegalArgumentException("Wrong function type:" + type);
