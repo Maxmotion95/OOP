6a3ebee4bd02a023a73f2c2699f9c4ede6a49205
SophyaL <sophyal@mail.ru>
2022-12-10 21:00:03 +0700

    add catching expressions

--- Task_1_5_1/src/main/java/ru/nsu/fit/lylova/Main.java
+++ Task_1_5_1/src/main/java/ru/nsu/fit/lylova/Main.java
@@ -3,9 +3,16 @@ package ru.nsu.fit.lylova;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String expression = sc.nextLine();
-        System.out.println(Calculator.calculateExpression(expression));
+        double result;
+        try {
+            result = Calculator.calculateExpression(expression);
+        } catch (Exception e) {
+            System.out.println("Something goes wrong: " + e.getMessage());
+            return;
+        }
+        System.out.println(result);
     }
 }
