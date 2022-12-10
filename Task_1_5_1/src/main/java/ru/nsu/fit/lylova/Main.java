f6d6c48e9b405134248e82c5136710484cdb8afd
SophyaL <sophyal@mail.ru>
2022-12-10 20:12:36 +0700

    add using of calculator in main

--- Task_1_5_1/src/main/java/ru/nsu/fit/lylova/Main.java
+++ Task_1_5_1/src/main/java/ru/nsu/fit/lylova/Main.java
@@ -1,7 +1,11 @@
 package ru.nsu.fit.lylova;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String expression = sc.nextLine();
+        System.out.println(Calculator.calculateExpression(expression));
     }
 }
