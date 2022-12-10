a8a590b27eef706b586a8fa32b26634669ddc37d
SophyaL <sophyal@mail.ru>
2022-12-10 15:06:10 +0700

    add CalculatorFunctionFactory with some CalculatorFunctions

---
+++ Task_1_5_1/src/main/java/ru/nsu/fit/lylova/CalculatorFunctionFactory.java
@@ -0,0 +1,26 @@
+package ru.nsu.fit.lylova;
+
+import ru.nsu.fit.lylova.functions.FunctionAdd;
+import ru.nsu.fit.lylova.functions.FunctionMultiply;
+import ru.nsu.fit.lylova.functions.FunctionSub;
+
+public class CalculatorFunctionFactory {
+    public CalculatorFunction createFunction(FunctionType type) {
+        CalculatorFunction function = null;
+
+        switch (type) {
+            case ADD:
+                function = new FunctionAdd();
+                break;
+            case SUB:
+                function = new FunctionSub();
+                break;
+            case MULTIPLY:
+                function = new FunctionMultiply();
+                break;
+            default:
+                throw new IllegalArgumentException("Wrong function type:" + type);
+        }
+        return function;
+    }
+}
---
+++ Task_1_5_1/src/main/java/ru/nsu/fit/lylova/functions/FunctionAdd.java
@@ -0,0 +1,20 @@
+package ru.nsu.fit.lylova.functions;
+
+import ru.nsu.fit.lylova.CalculatorFunction;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class FunctionAdd extends CalculatorFunction {
+    public FunctionAdd(){
+    }
+
+    @Override
+    public void produce(Stack<Double> calculatorStack) throws Exception {
+        if (calculatorStack.size() < 2)
+            throw new Exception("Not enough elements in calculatorStack");
+        double a = calculatorStack.pop();
+        double b = calculatorStack.pop();
+        calculatorStack.push(a + b);
+    }
+}
