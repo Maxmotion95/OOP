ee73dcdfb07d10faeb4ccdd49b1f6a54c975d135
SophyaL <sophyal@mail.ru>
2022-12-10 15:06:10 +0700

    add CalculatorFunctionFactory with some CalculatorFunctions

---
+++ Task_1_5_1/src/main/java/ru/nsu/fit/lylova/CalculatorFunction.java
@@ -0,0 +1,11 @@
+package ru.nsu.fit.lylova;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class CalculatorFunction {
+    public void produce(Stack<Double> calculatorStack) throws Exception {
+
+    }
+
+}
---
+++ Task_1_5_1/src/main/java/ru/nsu/fit/lylova/FunctionType.java
@@ -0,0 +1,13 @@
+package ru.nsu.fit.lylova;
+
+public enum FunctionType {
+    LOG,
+    POW,
+    SQRT,
+    SIN,
+    COS,
+    ADD,
+    SUB,
+    MULTIPLY,
+    DIVIDE
+}
