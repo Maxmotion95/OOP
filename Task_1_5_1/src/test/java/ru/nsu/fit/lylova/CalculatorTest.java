9c84aaf1a641296bae3ed8983c47aade4105fb65
SophyaL <sophyal@mail.ru>
2022-12-26 20:44:53 +0700

    add tests of addFunction

--- Task_1_5_1/src/test/java/ru/nsu/fit/lylova/CalculatorTest.java
+++ Task_1_5_1/src/test/java/ru/nsu/fit/lylova/CalculatorTest.java
@@ -2,6 +2,10 @@ package ru.nsu.fit.lylova;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
@@ -20,5 +24,85 @@ class CalculatorTest {
         assertEquals(3, calculator.calculateExpression("sqrt 9"));
         assertEquals(0, calculator.calculateExpression("log 1"));
         assertEquals(1, calculator.calculateExpression("pow -1 2"));
+    }
+
+    @Test
+    void testAddFunction() throws Exception {
+        Calculator calculator = new Calculator();
+        calculator.addFunction(new FunctionAddConstant(7));
+
+        assertEquals(7, calculator.calculateExpression("add_magic_constant 0"));
+
+        List<CalculatorFunction> functions = new ArrayList<>();
+        functions.add(new FunctionAddConstant(1007));
+        functions.add(new FunctionMultiplyByZero());
+        functions.add(new FunctionConstant37());
+
+        calculator = new Calculator(functions);
+
+        String expr = "add_magic_constant add_magic_constant multiply_by_zero const37";
+        assertEquals(2014, calculator.calculateExpression(expr));
+
+        calculator = new Calculator(new ArrayList<>());
+        calculator.addAllFunctions(functions);
+        assertEquals(2014, calculator.calculateExpression(expr));
+    }
+
+    private static class FunctionAddConstant implements CalculatorFunction {
+        double number;
+
+        FunctionAddConstant(double number) {
+            this.number = number;
+        }
+
+        @Override
+        public void produce(Stack<Double> calculatorStack) throws Exception {
+            if (calculatorStack.size() < 1) {
+                throw new Exception("Not enough elements in calculatorStack");
+            }
+            double a = calculatorStack.pop();
+            calculatorStack.push(a + number);
+        }
+
+        @Override
+        public boolean parse(String token) {
+            return token.equals("add_magic_constant");
+        }
+    }
+
+    private static class FunctionMultiplyByZero implements CalculatorFunction {
+
+        FunctionMultiplyByZero() {
+        }
+
+        @Override
+        public void produce(Stack<Double> calculatorStack) throws Exception {
+            if (calculatorStack.size() < 1) {
+                throw new Exception("Not enough elements in calculatorStack");
+            }
+            double a = calculatorStack.pop();
+            calculatorStack.push(a * 0);
+        }
+
+        @Override
+        public boolean parse(String token) {
+            return token.equals("multiply_by_zero");
+        }
+    }
+
+    private static class FunctionConstant37 implements CalculatorFunction {
+
+        FunctionConstant37() {
+        }
+
+        @Override
+        public void produce(Stack<Double> calculatorStack) throws Exception {
+            calculatorStack.push(37.0);
+        }
+
+        @Override
+        public boolean parse(String token) {
+            return token.equals("const37");
+        }
     }
 }
