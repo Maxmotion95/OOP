c3816069379b0901276429d170150e9464a74fc3
SophyaL <sophyal@mail.ru>
2022-12-27 13:14:02 +0700

    add calculator app implementation using picocli

--- Task_1_5_2/src/main/java/ru/nsu/fit/lylova/Main.java
+++ Task_1_5_2/src/main/java/ru/nsu/fit/lylova/Main.java
@@ -1,11 +1,6 @@
 package ru.nsu.fit.lylova;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import picocli.CommandLine;
 
 /**
  * Class Main with method main that implements the logic of the notebook console application.
@@ -13,91 +8,10 @@ import java.util.Date;
 public class Main {
     /**
      * The main method that implements the logic of the notebook console application.
+     *
      * @param args arguments of command line
      */
     public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Invalid command line arguments");
-            return;
-        }
-
-        Notebook notebook;
-
-        File dataFile = new File("data.json");
-        try {
-            notebook = new Notebook(new FileInputStream(dataFile));
-        } catch (FileNotFoundException e) {
-            notebook = new Notebook();
-        }
-
-        switch (args[0]) {
-            case "-add":
-                if (args.length != 3) {
-                    System.out.println("Invalid command line arguments. Correct format: -add title content");
-                    return;
-                }
-                notebook.addRecord(new NotebookRecord(args[1], args[2]));
-                break;
-            case "-show":
-                if (args.length == 1) {
-                    System.out.println(notebook.showAll());
-                    break;
-                }
-                if (args.length < 4) {
-                    System.out.println("Invalid command line arguments");
-                    return;
-                }
-                DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy hh:mm");
-                String[] keywords = new String[args.length - 3];
-                System.arraycopy(args, 3, keywords, 0, args.length - 3);
-
-                Date startDate;
-                try {
-                    startDate = dateFormat.parse(args[1]);
-                } catch (ParseException e) {
-                    System.out.println("Invalid command line arguments. Correct format of startDate: \"dd.MM.yyyy hh:mm\"");
-                    return;
-                }
-                Date endDate;
-                try {
-                    endDate =  dateFormat.parse(args[2]);
-                } catch (ParseException e) {
-                    System.out.println("Invalid command line arguments. Correct format of endDate: \"dd.MM.yyyy hh:mm\"");
-                    return;
-                }
-                System.out.println(notebook.showAllWithFilter(startDate, endDate, keywords));
-                break;
-            case "-rm":
-                if (args.length != 2) {
-                    System.out.println("Invalid command line arguments. Correct format: -rm title");
-                    return;
-                }
-                notebook.removeRecordsWithTitle(args[1]);
-                break;
-            default:
-                System.out.println("Invalid command line arguments.");
-                break;
-        }
-
-        FileOutputStream outputStream = null;
-        try {
-            outputStream = new FileOutputStream("data.json");
-        } catch (FileNotFoundException e) {
-            System.out.println("Loading changes broke :(((((((");
-            return;
-        }
-        byte[] strToBytes = notebook.getDataInJson().getBytes(StandardCharsets.UTF_8);
-        try {
-            outputStream.write(strToBytes);
-        } catch (IOException e) {
-            System.out.println("Loading changes broke :(((((((");
-            return;
-        }
-
-        try {
-            outputStream.close();
-        } catch (IOException e) {
-            System.out.println("Loading changes broke :(((((((");
-        }
+        new CommandLine(new NotebookApp("data.json")).execute(args);
     }
 }
---
+++ Task_1_5_2/src/main/java/ru/nsu/fit/lylova/NotebookApp.java
@@ -0,0 +1,157 @@
+package ru.nsu.fit.lylova;
+
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.ParentCommand;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Command(name = "notebook", mixinStandardHelpOptions = true, version = "checksum 0.1",
+        description = "A notebook with a set of functions: " +
+                "add an entry, delete an entry, " +
+                "display all entries sorted by the time of addition.",
+        subcommands = {
+                NotebookApp.AddCommand.class,
+                NotebookApp.RmCommand.class,
+                NotebookApp.ShowCommand.class})
+class NotebookApp {
+    private Notebook notebook;
+    private String notebookFilePath;
+    NotebookApp(String notebookFilePath) {
+        this.notebookFilePath = notebookFilePath;
+        File dataFile = new File(notebookFilePath);
+        try {
+            notebook = new Notebook(new FileInputStream(dataFile));
+        } catch (FileNotFoundException e) {
+            notebook = new Notebook();
+        }
+    }
+
+    public void loadChanges() {
+        FileOutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream(notebookFilePath);
+        } catch (FileNotFoundException e) {
+            System.out.println("Loading changes broke :(((((((");
+            return;
+        }
+        byte[] strToBytes = notebook.getDataInJson().getBytes(StandardCharsets.UTF_8);
+        try {
+            outputStream.write(strToBytes);
+        } catch (IOException e) {
+            System.out.println("Loading changes broke :(((((((");
+            return;
+        }
+
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+            System.out.println("Loading changes broke :(((((((");
+        }
+    }
+
+    @Command(name = "-add", mixinStandardHelpOptions = true,
+            description = "Add record with specified title and content. \n" +
+                    "First parameter is title. Second is content of record.")
+    static class AddCommand implements Runnable {
+
+        @ParentCommand
+        NotebookApp parent;
+
+        @Option(names = "-add")
+        public boolean add;
+
+        @Parameters(index = "0")
+        public String title;
+        @Parameters(index = "1")
+        public String content;
+
+        @Override
+        public void run() {
+            parent.notebook.addRecord(new NotebookRecord(title, content));
+            parent.loadChanges();
+        }
+    }
+
+    @Command(name = "-rm", mixinStandardHelpOptions = true,
+            description = "Remove all records with specified title.\n" +
+                    "First parameter is title.")
+    static class RmCommand implements Runnable {
+        @ParentCommand
+        NotebookApp parent;
+
+        @Option(names = "-rm")
+        public boolean rm;
+
+        @Parameters(index = "0")
+        public String title;
+
+        @Override
+        public void run() {
+            parent.notebook.removeRecordsWithTitle(title);
+            parent.loadChanges();
+        }
+    }
+
+    @Command(name = "-show", mixinStandardHelpOptions = true,
+            description = "Show all records if no parameters or show filtered records. \n" +
+                    "To show filtered records first and second parameters must be dates of format \"dd.MM.yyyy hh:mm\". " +
+                    "First is minimal date of creation and second is maximal date of creation. " +
+                    "The next parameters will be list of keywords which title of record must contain.")
+    static class ShowCommand implements Runnable {
+        @ParentCommand
+        NotebookApp parent;
+
+        @Option(names = "-show")
+        public boolean rm;
+
+        @Parameters
+        public String[] params;
+
+        @Override
+        public void run() {
+            if (params == null) {
+                System.out.println(parent.notebook.showAll());
+                return;
+            }
+
+            if (params.length < 2) {
+                System.err.println("Invalid command line arguments. First 2 parameters must be starDate and endDate");
+                return;
+            }
+
+            DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy hh:mm");
+            String[] keywords = new String[params.length - 2];
+            System.arraycopy(params, 2, keywords, 0, params.length - 2);
+
+            Date startDate;
+            try {
+                startDate = dateFormat.parse(params[0]);
+            } catch (ParseException e) {
+                System.err.println("Invalid command line arguments. Correct format of startDate: \"dd.MM.yyyy hh:mm\"");
+                return;
+            }
+            Date endDate;
+            try {
+                endDate = dateFormat.parse(params[1]);
+            } catch (ParseException e) {
+                System.err.println("Invalid command line arguments. Correct format of endDate: \"dd.MM.yyyy hh:mm\"");
+                return;
+            }
+            System.out.println(parent.notebook.showAllWithFilter(startDate, endDate, keywords));
+        }
+    }
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "display a help message")
+    private boolean helpRequested = false;
+}
