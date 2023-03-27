eeeee3d027272af0e7d0dc741fc93ef58c6131d6
SophyaL <sophyal@mail.ru>
2023-03-28 01:06:55 +0700

    add CourierService

--- Task_2_2_1/src/main/java/ru/nsu/fit/lylova/Main.java
+++ Task_2_2_1/src/main/java/ru/nsu/fit/lylova/Main.java
@@ -3,26 +3,31 @@ package ru.nsu.fit.lylova;
 import ru.nsu.fit.lylova.environment.PizzaOrder;
 import ru.nsu.fit.lylova.environment.PizzaWarehouse;
 import ru.nsu.fit.lylova.environment.PizzeriaOrderQueue;
+import ru.nsu.fit.lylova.services.CourierService;
 import ru.nsu.fit.lylova.staff.bakers.NormalBaker;
 import ru.nsu.fit.lylova.staff.couriers.Courier;
 import ru.nsu.fit.lylova.staff.couriers.LazyCourier;
 
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         Logger log = Logger.getLogger(Main.class.getName());
         log.info("logging started");
 
         PizzeriaOrderQueue orderQueue = new PizzeriaOrderQueue();
         PizzaWarehouse warehouse = new PizzaWarehouse(3);
+
+        CourierService courierService = new CourierService(warehouse, log, "Task_2_2_1/src/main/resources/couriersConfig.json");
         NormalBaker b1 = new NormalBaker(orderQueue, warehouse, "VASYA", log, 2000);
         b1.start();
         NormalBaker b2 = new NormalBaker(orderQueue, warehouse, "ANTON", log, 3000);
         b2.start();
-        Courier c1 = new LazyCourier(5000, 3, warehouse, "OLEG", log);
-        c1.start();
+//        Courier c1 = new LazyCourier(warehouse, "OLEG", log, 5000, 3);
+//        c1.start();
+        courierService.startAll();
 
         Scanner sc = new Scanner(System.in);
         int pastOrderId = 0;
@@ -33,16 +38,16 @@ public class Main {
                 b2.softShutdown();
                 b1.join();
                 b2.join();
-                c1.softShutdown();
-                c1.join();
+                courierService.softShutdownAll();
+                courierService.joinAll();
                 break;
             } else if (s.equals("forceShutdown") || s.equals("f")) {
                 b1.forceShutdown();
                 b2.forceShutdown();
-                c1.forceShutdown();
+                courierService.forceShutdownAll();
                 b1.join();
                 b2.join();
-                c1.join();
+                courierService.joinAll();
                 break;
             } else {
                 orderQueue.addOrder(new PizzaOrder(pastOrderId));
